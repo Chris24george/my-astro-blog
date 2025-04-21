0156ef97c36c566a5e7f414c1c7a406f8a1abbc5
Chris24george <chris24george@gmail.com>
2025-04-21 12:53:01 -0700

    revamp home page styling

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,11 +12,11 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
   const { title, pubDatetime, modDatetime, description } = frontmatter;
 
   const headerProps = {
-    className: "text-lg font-medium decoration-dashed hover:underline",
+    className: "text-[1.05rem] font-medium decoration-dashed hover:underline",
   };
 
   return (
-    <li className="my-6">
+    <li className="mb-3 bg-skin-card border border-skin-line rounded-lg shadow-sm p-4 w-80 h-36">
       <a
         href={href}
         className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
@@ -28,7 +28,7 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
         )}
       </a>
       <Datetime pubDatetime={pubDatetime} modDatetime={modDatetime} />
-      <p>{description}</p>
+      <p className="text-[0.98rem]">{description}</p>
     </li>
   );
 }
